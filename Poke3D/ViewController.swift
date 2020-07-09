bf95b6fa7fce9e4cf5be279e02770efa0ea0718d
Olga <ameth.olga@gmail.com>
2020-07-10 00:49:07 +0300

    First commit

--- Poke3D/ViewController.swift
+++ Poke3D/ViewController.swift
@@ -20,22 +20,23 @@ class ViewController: UIViewController, ARSCNViewDelegate {
         // Set the view's delegate
         sceneView.delegate = self
         
-        // Show statistics such as fps and timing information
-        sceneView.showsStatistics = true
-        
-        // Create a new scene
-        let scene = SCNScene(named: "art.scnassets/ship.scn")!
-        
-        // Set the scene to the view
-        sceneView.scene = scene
+        sceneView.autoenablesDefaultLighting = true
     }
     
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
         
         // Create a session configuration
-        let configuration = ARWorldTrackingConfiguration()
+        let configuration = ARImageTrackingConfiguration()
 
+        if let imageToTrack = ARReferenceImage.referenceImages(inGroupNamed: "Pokemon Cards", bundle: Bundle.main) {
+        
+            configuration.trackingImages = imageToTrack
+            configuration.maximumNumberOfTrackedImages = 2
+            
+            print("Images successfully added")
+        
+        }
         // Run the view's session
         sceneView.session.run(configuration)
     }
@@ -49,27 +50,62 @@ class ViewController: UIViewController, ARSCNViewDelegate {
 
     // MARK: - ARSCNViewDelegate
     
-/*
-    // Override to create and configure nodes for anchors added to the view's session.
     func renderer(_ renderer: SCNSceneRenderer, nodeFor anchor: ARAnchor) -> SCNNode? {
+            
         let node = SCNNode()
-     
+        
+        DispatchQueue.main.async {
+            
+            if let imageAnchor = anchor as? ARImageAnchor {
+                
+                let plane = SCNPlane(width: imageAnchor.referenceImage.physicalSize.width, height: imageAnchor.referenceImage.physicalSize.height)
+                
+                plane.firstMaterial?.diffuse.contents = UIColor(white: 1.0, alpha: 0.5)
+                
+                let planeNode = SCNNode(geometry: plane)
+                
+                planeNode.eulerAngles.x = -.pi/2
+                
+                node.addChildNode(planeNode)
+                
+                if imageAnchor.referenceImage.name == "oddish" {
+                    
+                    if let pokeScene = SCNScene(named: "art.scnassets/oddish.scn") {
+                        
+                        if let pokeNode = pokeScene.rootNode.childNodes.first {
+                            
+                            pokeNode.eulerAngles.x = .pi / 2
+                            
+                            planeNode.addChildNode(pokeNode)
+                            
+                        }
+                        
+                    }
+                    
+                }
+                
+                if imageAnchor.referenceImage.name == "eevee" {
+                    
+                    if let pokeScene = SCNScene(named: "art.scnassets/eevee.scn") {
+                        
+                        if let pokeNode = pokeScene.rootNode.childNodes.first {
+                            
+                            pokeNode.eulerAngles.x = .pi / 2
+                            
+                            planeNode.addChildNode(pokeNode)
+                            
+                        }
+                        
+                    }
+                    
+                }
+                
+            }
+            
+        }
+        
         return node
-    }
-*/
-    
-    func session(_ session: ARSession, didFailWithError error: Error) {
-        // Present an error message to the user
         
     }
     
-    func sessionWasInterrupted(_ session: ARSession) {
-        // Inform the user that the session has been interrupted, for example, by presenting an overlay
-        
-    }
-    
-    func sessionInterruptionEnded(_ session: ARSession) {
-        // Reset tracking and/or remove existing anchors if consistent tracking is required
-        
-    }
 }
